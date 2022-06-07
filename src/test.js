a43c8f05129a9d7b678ff692c08b72c1587bc34b
blckclov3r <blckclov3r@gmail.com>
2022-06-07 18:41:53 +0800

    removed comment

--- src/test.js
+++ src/test.js
@@ -15,8 +15,8 @@ Student.prototype.report = function(){
 let student1 = new Student('Marcus');
 let student2 = new Student('John');
 
-student1.study();  //decrease hp by 1 and increase int by 1
-student1.study();  //decrease hp by 1 and increase int by 1
+student1.study();  
+student1.study();  
 
-student1.report();  //have it log 'Marcus has hp of 8 and int of 12'
-student2.report();  //have it log 'John has hp of 10 and int of 10'
+student1.report(); 
+student2.report();  
