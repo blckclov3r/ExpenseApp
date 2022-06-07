d05c2e8666a075f2fb3e1908b2583608892a2be8
blckclov3r <blckclov3r@gmail.com>
2022-06-07 21:29:27 +0800

    commit

--- src/test.js
+++ src/test.js
@@ -17,17 +17,6 @@ let student2 = new Student('John');
 
 student1.study();  
 student1.study();  
-<<<<<<< HEAD
 
-<<<<<<< HEAD
-student1.report();  
-student2.report(); 
-=======
 student1.report();  //asa
 student2.report();  //ambot
->>>>>>> newbranch
-=======
-
-
-
->>>>>>> hellopo
